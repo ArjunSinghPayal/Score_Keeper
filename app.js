8d578c0f94423e04e5b69cd16b0e09691b792d6d
ArjunSinghPayal <arjunsinghpayal7@gmail.com>
2025-04-02 00:24:50 +0530

    finished the porject and also removed redundant code for js file and shorten some logic using for_of loop

--- app.js
+++ app.js
@@ -1,13 +1,19 @@
-const p1button = document.querySelector('#p1button');
-const p2button = document.querySelector('#p2button');
+const p1 = {
+    score: 0,
+    button: document.querySelector('#p1button'),
+    display: document.querySelector('#p1Display')
+}
+
+const p2 = {
+    score: 0,
+    button: document.querySelector('#p2button'),
+    display: document.querySelector('#p2Display')
+}
+
 const resetBtn = document.querySelector('#reset');
-const p1Display = document.querySelector('#p1Display');
-const p2Display = document.querySelector('#p2Display');
 const winScoreSelector= document.querySelector('#playto');
 
 let winningScore = 3;
-let p1Score = 0;
-let p2Score = 0;
 let isGameOver = false;
 winScoreSelector.value=3;
 
@@ -19,33 +25,38 @@ winScoreSelector.addEventListener('change',() =>{
 })
 
 
-p1button.addEventListener('click', () => {
-    if (!isGameOver) {
-        p1Score += 1;
-        if (p1Score === winningScore) {
+function updateScores(player, opponent){
+        if (!isGameOver) {
+        player.score += 1;
+        if (player.score === winningScore) {
             isGameOver = true;
+            player.display.classList.add('has-text-success');
+            opponent.display.classList.add('has-text-danger');
+            player.button.disabled=true;
+            opponent.button.disabled=true;
         }
-        p1Display.textContent = p1Score;
+        player.display.textContent = player.score;
     }
+}
+
+
+p1.button.addEventListener('click', () => {
+    updateScores(p1,p2)
 })
 
 
-p2button.addEventListener('click', () => {
-    if (!isGameOver) {
-        p2Score += 1;
-        if (p2Score === winningScore) {
-            isGameOver = true;
-        }
-        p2Display.textContent = p2Score;
-    }
+p2.button.addEventListener('click', () => {
+    updateScores(p2,p1)
 })
 
 resetBtn.addEventListener('click', reset)
 
 function reset() {
     isGameOver = false;
-    p1Score = 0;
-    p2Score = 0;
-    p1Display.textContent = 0;
-    p2Display.textContent = 0;
+    for(p of [p1,p2]){
+        p.score = 0;
+        p.display.textContent = 0;
+        p.display.classList.remove('has-text-success','has-text-danger');
+        p.button.disabled=false;
+    }
 }
